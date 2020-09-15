d4fc5535a5f46ad535e9abaf83f712c3907e6509
goutham kallempudi <kallempudigoutham@gmail.com>
2020-09-15 09:45:43 +0530

    conditional snippets

---
+++ JavaScript Code Snipets/conditions.js
@@ -0,0 +1,10 @@
+const age  = 35;
+if(age > 50){
+  console.log("old");
+}
+else if(age > 30 && age <= 50){
+  console.log("Mid Age");
+}
+else{
+  console.log("young");
+}
---
+++ JavaScript Code Snipets/switch.js
@@ -0,0 +1,12 @@
+let fruit = "apple";
+switch(fruit){
+  case "banana":
+    console.log("banana");
+    break;
+  case "apple":
+    console.log("apple");
+    break;
+  default:
+    console.log("Not a fruit");
+    break;
+}
