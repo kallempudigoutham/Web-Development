b8ea3c72276dbf18d6c34ffdf5ac413f667ba335
goutham kallempudi <kallempudigoutham@gmail.com>
2020-09-24 13:40:41 +0530

    Added more routes and nodemon installation

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,18 @@ app.get('/', function(req, res){
    res.send('<h1> Hello World </h1>');
 });
 
+app.get('/contact' , function(req, res){
+   res.send('Please contact me at: gouthammanit@gmail.com');
+});
+
+app.get('/about' , function(req, res){
+   res.send('This is me Goutham');
+});
+
+app.get('/hobbies' , function(req, res){
+   res.send('<ul><li> Cricket </li></ul>')
+});
+
 app.listen(3000 , function(){
    console.log("Server started on 3000");
 });
