83554907936100436b96a8b10ff926d5fd63edee
goutham kallempudi <kallempudigoutham@gmail.com>
2020-09-30 12:32:43 +0530

    Calculaor webapplicaton

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended : true}));
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + "/index.html");
 })
+
+app.post('/', function(req, res){
+    num1 = Number(req.body.num1);
+    num2 = Number(req.body.num2);
+    result = num1 + num2;
+    res.send("The result is " + result);
+});
 
 app.listen('3000' , function(){
   console.log('Server started at port 3000');
