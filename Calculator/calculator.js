cfba3c4b185f243c01c362d1f5633107b6fe6987
goutham kallempudi <kallempudigoutham@gmail.com>
2020-09-30 12:48:36 +0530

    BMI Calculator web app

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,13 +6,24 @@ app.use(bodyParser.urlencoded({ extended : true}));
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.post('/', function(req, res){
     num1 = Number(req.body.num1);
     num2 = Number(req.body.num2);
     result = num1 + num2;
     res.send("The result is " + result);
+});
+
+app.get('/bmicalculator', function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post('/bmicalculator', function(req, res){
+  height = Number(req.body.height);
+  weight = Number(req.body.weight);
+  bmi = ( weight /(height * height));
+  res.send("Your BMI is " + bmi);
 });
 
 app.listen('3000' , function(){
